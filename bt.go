c52245c7b8089203477c186418385b5998472686
erleene <erleene@beamery.com>
2017-11-10 12:36:16 +0000

    building out binary tree

---
+++ bt.go
@@ -0,0 +1,29 @@
+package main
+
+
+
+type Tree struct {
+  Left int
+  Right int
+  value string
+}
+
+
+//create a recursive function to insert to the Tree
+
+//create an empty Tree
+
+func (t *Tree) Insert(val int) {
+  //we want to insert a value to the tree
+  }
+
+
+
+
+func main() {
+
+  newTree := &Tree(2, 4)
+
+  fmt.Printf(*newTree)
+
+}
