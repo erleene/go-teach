74d119f5a6d4ca2603b10c4968b48df6077ae5b2
erleene <erleene@beamery.com>
2017-11-15 12:30:39 +0000

    adding more comments in

--- routines/routines.go
+++ routines/routines.go
@@ -28,25 +28,23 @@ func printURLBody(url *Url, wg *sync.WaitGroup) error {
 	println(string(output))
 	fmt.Printf("%v", *response)
 
-	wg.Done()
+	wg.Done() //calls when it's done
 	return nil
 }
 
 func main() {
 
-	//create array of urls
-	urls := []*Url{&Url{Okay: true, URL: "https://preview.beamery.com", Body: ""},
+	//create array of urls: urls is now an array pointer
+	urls := []*Url{
+		&Url{Okay: true, URL: "https://preview.beamery.com", Body: ""},
 		&Url{Okay: true, URL: "https://canary.beamery.com", Body: ""},
-		&Url{Okay: true, URL: "https://beamery.com", Body: ""}} //pointer array
-	//urls := &UrlLink{{Link: *url1}, {Link: *url2}, {Link: *url3}}
-
-	//urls := []string{"https://preview.beamery.com", "https://canary.beamery.com", "https://beamery.com"}
+		&Url{Okay: true, URL: "https://beamery.com", Body: ""}}
 
 	var wg sync.WaitGroup
 
 	for x := 0; x < len(urls); x++ {
 		currentURL := urls[0]
-		wg.Add(1)
+		wg.Add(1)                        //add to waitgroup counter
 		go printURLBody(currentURL, &wg) //pass waitgroup to go routine
 	}
 
