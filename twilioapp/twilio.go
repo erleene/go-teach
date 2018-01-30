e8e905a019e79627aaed7d5507e03e9f82ebd075
erleene <erleene@beamery.com>
2018-01-30 14:47:05 +0000

    twilio app

---
+++ twilioapp/twilio.go
@@ -0,0 +1,45 @@
+// Copyright 2015 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Sample twilio demonstrates sending and receiving SMS, receiving calls via Twilio from App Engine flexible environment.
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+)
+
+// [START import]
+import (
+	"bitbucket.org/ckvist/twilio/twirest"
+)
+
+func main() {
+
+	http.HandleFunc("/sms/send", sendSMSHandler)
+	appengine.Main()
+}
+
+func sendSMSHandler(w http.ResponseWriter, r *http.Request) {
+	accountSid := "ACc0160b769e60c25c7d68d8b9cd54ae9f"
+	authToken := "8904837cb3aac85835f9eba0314618e1"
+
+	client := twirest.NewClient(accountSid, authToken)
+
+	msg := twirest.SendMessage{
+		Text: "Hello! Golang POC Twilio app deployed: works!",
+		From: "+441536609533",
+		To:   "+447506503540",
+	}
+
+	resp, err := client.Request(msg)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not send SMS: %v", err), 500)
+		return
+	}
+
+	fmt.Fprintf(w, "SMS sent successfully. Response:\n%#v", resp.Message)
+}
